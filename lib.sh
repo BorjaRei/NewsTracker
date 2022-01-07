dce4512c49b7862875339e1934ab2bac8cf0e79b
BorjaRei <49234041+BorjaRei@users.noreply.github.com>
2022-01-07 12:11:57 +0100

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -9,4 +9,5 @@ pip install pandas==1.3.5
 pip install newsdataapi==0.1.1
 pip install sklearn==0.0
 pip install transformers==4.15.0
+pip install elasticsearch==7.16.2
 pip3 install torch==1.8.2+cpu torchvision==0.9.2+cpu torchaudio==0.8.2 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
